bc117e22b584966ef459a5576e87dab6940214ca
singhalharsh10 <hsinghal1_be18@thapar.edu>
2020-07-20 11:50:42 +0530

    password and specific user msg added

--- public/script.js
+++ public/script.js
@@ -1,29 +1,75 @@
 const socket = io()
 
-let btnSend = document.getElementById('btnSend')
-let inpMsg = document.getElementById('inpMsg')
-let ulMsgList = document.getElementById('ulMsgList')
+// let btnSend = document.getElementById('btnSend')
+// let inpMsg = document.getElementById('inpMsg')
+// let ulMsgList = document.getElementById('ulMsgList')
 
 
-btnSend.onclick = function() {
-    socket.emit('MsgSend', {
-        msg: inpMsg.value
+// btnSend.onclick = function() {
+//     socket.emit('MsgSend', {
+//         msg: inpMsg.value
+//     })
+//     inpMsg.value
+// }
+
+// // This function will add msg emit from io from server.js onto all users or all parallel local host webpage
+// socket.on('msg_rcvd', (data) => {
+//     let linewMsg = document.createElement('li')
+//     linewMsg.innerText = data.msg
+//     ulMsgList.appendChild(linewMsg)
+// })
+
+
+
+// // // messgae from server to client
+// // socket.on('whizz', () => {
+// //     let div = document.createElement('div')
+// //     div.innerText = 'whizz'
+// //     document.body.appendChild(div)
+// // })
+
+
+// Writing code in Jquery
+
+
+
+
+$('#loginBox').show()
+    // We have hide chatbox until login is done
+$('#chatBox').hide()
+
+// Sending username and pass to server
+$('#btnstart').click(() => {
+    socket.emit('login', {
+        username: $('#inpUserName').val(),
+        password: $('#inpPass').val(),
+
     })
-    inpMsg.value
-}
-
-// This function will add msg emit from io from server.js onto all users or all parallel local host webpage
-socket.on('msg_rcvd', (data) => {
-    let linewMsg = document.createElement('li')
-    linewMsg.innerText = data.msg
-    ulMsgList.appendChild(linewMsg)
 })
 
 
+socket.on('logged_in', () => {
+    $('#loginBox').hide()
+    $('#chatBox').show()
+})
 
-// // messgae from server to client
-// socket.on('whizz', () => {
-//     let div = document.createElement('div')
-//     div.innerText = 'whizz'
-//     document.body.appendChild(div)
-// })
+socket.on('login-failed', () => {
+    window.alert('Username or password wrong')
+})
+
+
+$('#btnSendMsg').click(() => {
+    socket.emit('msg_send', {
+        // for sendib msg to a particular user
+        to: $('#inpToUser').val(),
+        msg: $('#inpNewMsg').val()
+    })
+
+})
+
+// executed after server emit recvd msg
+socket.on('msg_rcvd', (data) => {
+    $('#ulMsg').append($('<li>').text(
+        `[${data.from}]: ${data.msg}`
+    ))
+})
--- server.js
+++ server.js
@@ -2,22 +2,70 @@ const express = require('express')
 const http = require('http')
 const app = express()
 const socketio = require('socket.io')
+const { Socket } = require('net')
 
 
 const server = http.createServer(app)
 const io = socketio(server)
 
+let users = {
+    swati: 'va34'
+}
+
+let socketMap = {}
+
 io.on('connection', (socket) => {
     console.log('connected with socket id', socket.id)
-        // data is the msg send from script.js
-    socket.on('MsgSend', (data) => {
-        // console.log('received', data.msg)
-        // if we have done below socket.emit then it would hve passed msg to all users instead ye sirf usi html pagr pr ul add krta
-        // io.emit will send msg to all including sender also
-        // socket.broadcast.emit will send to all except sender
-        socket.broadcast.emit('msg_rcvd', data)
 
+    function login(s, u) { //s=socket,u=username
+        s.join(u)
+        s.emit('logged_in')
+        socketMap[s.id] = u
+        console.log(socketMap)
+    }
+
+    // code for learning
+    // data is the msg send from script.js
+    // socket.on('MsgSend', (data) => {
+    //     // console.log('received', data.msg)
+    //     // if we have done below socket.emit then it would hve passed msg to all users instead ye sirf usi html pagr pr ul add krta
+    //     // io.emit will send msg to all including sender also
+    //     // socket.broadcast.emit will send to all except sender
+    //     socket.broadcast.emit('msg_rcvd', data)
+
+
+
+
+    // })
+    // final code
+    socket.on('login', (data) => {
+        // checking if username already exist in our users array
+        if (users[data.username]) {
+            if (users[data.username] == data.password) {
+                login(socket, data.username)
+            } else {
+                socket.emit('login-failed')
+            }
+        } else {
+            // new user
+            users[data.username] = data.password
+            login(socket, data.username)
+
+
+        }
+        console.log(users)
     })
+
+    // Below we are using the logic that if msg_received from client to server has a to argument it will send msg to particular user else will send to all user
+    socket.on('msg_send', (data) => {
+        data.from = socketMap[socket.id]
+        if (data.to) {
+            io.to(data.to).emit('msg_rcvd', data)
+        } else {
+            socket.broadcast.emit('msg_rcvd', data)
+        }
+    })
+
 })
 
 
@@ -25,7 +73,7 @@ io.on('connection', (socket) => {
 
 
 
-app.use(express.static(__dirname + '/public'))
+app.use('/', express.static(__dirname + '/public'))
 
 
 server.listen(3000, () => {
